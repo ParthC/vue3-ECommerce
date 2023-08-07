71b6dc18e07ab8a4ec8e305d655ab0ea8c109987
DESKTOP-OADVGBB\Parth <parth@reshiftmedia.com>
2023-08-07 18:25:08 -0400

    project setup

---
+++ src/mochdata/item.js
@@ -0,0 +1,11 @@
+const items = [
+    {
+        id: 1,
+        name: "test",
+        price: 15.00,
+        category: 'Sports',
+        description: 'Running shoes'
+    }
+]
+
+export default items
---
+++ src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+  
+  </div>
+</template>
+
+<script>
+import items from '../mochdata/item.js'
+
+export default {
+  name: 'HomeView',
+  components: {
+    
+  },
+  data() {
+    return {
+      product: items
+    }
+  }
+}
+</script>
+../mochdata/items.js
