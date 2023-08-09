3b22435b5ee307596e2fad1a2079c16377cd03fd
DESKTOP-OADVGBB\Parth <parth@reshiftmedia.com>
2023-08-08 21:09:07 -0400

    adding cart

---
+++ src/components/products/ProductSummaryCard.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="card">
+        <h3> {{ product.name }} </h3>
+        <h5 class="price"> Price: ${{ product.price.toFixed(2) }} </h5>
+        <p class="description"> Desscription: {{ product.description }}</p>
+        <p class="text"> {{  product.category }}</p>
+        <button class="view-product-button" @click="$emit('view-product', product)"> View </button>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['product'],
+    computed: {
+        description() {
+            return this.product.description.substring(0, 150)
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+ .card {
+    width: 80%;
+    margin: 10%;
+    padding: 1rem;
+    border-radius: 0.5rem;
+    background-color: white;
+    box-shadow: 0 0 5px gray;
+ 
+    h5.price {
+    color: gray;
+    }
+
+    p.description {
+        font-size: .85rem;
+    }
+
+    p.text {
+        color: gray;
+    }
+
+    button.view-product-button {
+        padding: 1rem;
+        background-color: rgb(76, 160, 187);
+        border: none;
+        color: white;
+        font-weight: bold;
+        font-size: 1rem;
+        border-radius: 5px;
+        cursor: pointer;
+    }
+}
+
+@media (min-width: 500px) {
+    .card {
+        width: 350px;
+        margin: 10px;
+    }
+}
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,22 +1,49 @@
 <template>
   <div class="home">
-  
+    <Drawer 
+    :product="product"
+    :active="active.product_drawer"    
+    />
+    <div class="product-card-container">
+      <ProductSummaryCard v-for="product in items" :key="product.id" :product="product"
+      v-on:view-product="viewProduct($event)"/>
+    </div>
   </div>
 </template>
 
 <script>
 import items from '../mochdata/item.js'
+import ProductSummaryCard from '../components/products/ProductSummaryCard.vue'
+import Drawer from '../components/products/ProductDescription.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    
+    ProductSummaryCard,
+    Drawer
   },
   data() {
     return {
-      product: items
+      product: null,
+      items: items,
+      active: {
+        product_drawer: false
+      }
+    }
+  },
+  methods:{
+    viewProduct(product) {
+      this.product = product
     }
   }
 }
 </script>
-../mochdata/items.js
+
+<style lang="scss">
+
+.product-card-container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+</style>
